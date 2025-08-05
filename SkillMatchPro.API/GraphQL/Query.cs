1d8ea497e30175a4e9b22fe4e25ce3a2f37d8b30
igorntwari <igorntwari28@gmail.com>
2025-08-05 21:55:21 +0200

    feat: setup db and graphql

---
+++ SkillMatchPro.API/GraphQL/Query.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillMatchPro.Domain.Entities;
+using SkillMatchPro.Infrastructure.Data;
+
+namespace SkillMatchPro.API.GraphQL;
+
+public class Query
+{
+    public string Hello() => "Hello from SkillMatch Pro!";
+
+    public async Task<List<Employee>> GetEmployees([Service] ApplicationDbContext context)
+    {
+        return await context.Employees.ToListAsync();
+    }
--- SkillMatchPro.API/Program.cs
+++ SkillMatchPro.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SkillMatchPro.API.GraphQL;
+using SkillMatchPro.Infrastructure.Data;
 
 namespace SkillMatchPro.API;
 
@@ -7,16 +10,23 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddAuthorization();
-
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        // Add services
+        builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
+        // Add GraphQL
+        builder.Services
+            .AddGraphQLServer()
+            .AddQueryType<Query>();
+
+        // Add PostgreSQL
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+        // Configure pipeline
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -24,28 +34,9 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
+        app.MapGraphQL();
+        app.MapControllers();
 
         app.Run();
     }
